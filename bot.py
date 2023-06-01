5fe67f8749bd3475e15106fae790508625055647
lsdumas <119813587+lsdumas@users.noreply.github.com>
2023-06-01 19:15:16 +0200

    Added, TAF fetcher and METAR fallback

--- bot.py
+++ bot.py
@@ -19,18 +19,41 @@ async def on_ready():
         print(e)
 
 @bot.tree.command(name = 'metar', description = 'Recherche le dernier METAR disponible d\'un aéroport.')
-@app_commands.describe(icao='ICAO')
+@app_commands.describe(icao = 'ICAO')
 async def metar(interaction: discord.Interaction, icao:str):
 
     url = f'https://api.checkwx.com/metar/{icao}/decoded?x-api-key=92da5b1db838401d8f67720db4'
     response = requests.get(url)
     metarjson = response.json()
-    airport = metarjson["data"][0]['station']['name']
-    metarraw = metarjson["data"][0]['raw_text']   
-    print(airport)
-    print(metarraw)
-    embed = discord.Embed(title=f'{airport}', color=0x2483c5)
-    embed.add_field(name='METAR', value=f'`{metarraw}`')
+    valide = metarjson['results']
+    if valide == 0:
+        url = f'https://api.checkwx.com/metar/{icao}/nearest/decoded?x-api-key=92da5b1db838401d8f67720db4'
+        response = requests.get(url)                                                                            #to be completed
+        valide2 = metarjson['result']                                                                              #validation 2nd ICAO if 1st invalid
+        airport = metarjson["data"][0]['station']['name']
+        metarraw = metarjson["data"][0]['raw_text']   
+        embed = discord.Embed(title = f'METAR le plus proche de {icao.upper()}', color = 0x2483c5)
+        embed.add_field(name = f'{airport}', value = f'`{metarraw}`')
+        await interaction.response.send_message(embed = embed)
+    else:     
+        airport = metarjson["data"][0]['station']['name']
+        metarraw = metarjson["data"][0]['raw_text']   
+        embed = discord.Embed(title = f'{airport}', color = 0x2483c5)
+        embed.add_field(name = 'METAR', value = f'`{metarraw}`')
+        await interaction.response.send_message(embed = embed)
+
+
+@bot.tree.command(name = 'taf', description = 'Recherche le TAF d\' un aéroport')
+@app_commands.describe(icao = 'ICAO')
+async def taf(interaction: discord.Interaction, icao:str):
+    
+    url = f'https://api.checkwx.com/taf/{icao}/decoded?x-api-key=92da5b1db838401d8f67720db4'
+    response = requests.get(url)
+    tafjson = response.json()
+    airport = tafjson["data"][0]['station']['name']
+    rawtaf = tafjson["data"][0]['raw_text']
+    embed = discord.Embed(title = f'{airport}', color=0x2483c5)
+    embed.add_field(name='TAF', value=f'`{rawtaf}`')
     await interaction.response.send_message(embed=embed)
 
 bot.run(TOKEN)
